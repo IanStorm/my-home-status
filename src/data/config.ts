ba52c36a0ee83a4bd28858fd67a33a6c307e7db0
Christian Sporer <12594177+IanStorm@users.noreply.github.com>
2025-04-21 18:05:02 +0200

    refactor: Adapt grouping and naming

--- src/data/config.ts
+++ src/data/config.ts
@@ -10,7 +10,8 @@ import {
 
 /* no export */ const GROUP_01 = "01 - my-home-dns";
 /* no export */ const GROUP_02 = "02 - my-smart-home";
-/* no export */ const GROUP_03 = "03 - domains";
+/* no export */ const GROUP_03 = "03 - .de domain";
+/* no export */ const GROUP_04 = "04 - .me domain";
 
 export const myGatusConfig: Configuration = {
 	alerting: {
@@ -24,68 +25,67 @@ export const myGatusConfig: Configuration = {
 		},
 	},
 	endpoints: [
-		createIsAliveEndpointCfg({ group: GROUP_01, name: "pihole", url: "http://my-home-dns/admin" }),
+		createIsAliveEndpointCfg({ group: GROUP_01, name: "pihole (ping)", url: "http://my-home-dns/admin" }),
 
-		createIsAliveEndpointCfg({ group: GROUP_02, name: "cctv-nestbox", url: "http://my-home-cctv-nestbox" }),
-		createIsAliveEndpointCfg({ group: GROUP_02, name: "mqtt", url: "http://my-smart-home:8083" }),
-		createIsAliveEndpointCfg({ group: GROUP_02, name: "nginx", url: "http://my-smart-home" }),
-		createIsAliveEndpointCfg({ group: GROUP_02, name: "node-red", url: "http://my-smart-home:1880/ui" }),
-		createIsAliveEndpointCfg({ group: GROUP_02, name: "zigbee2mqtt", url: "http://my-smart-home:8099" }),
+		createIsAliveEndpointCfg({ group: GROUP_02, name: "cctv-nestbox (ping)", url: "http://my-home-cctv-nestbox" }),
+		createIsAliveEndpointCfg({ group: GROUP_02, name: "mqtt (ping)", url: "http://my-smart-home:8083" }),
+		createIsAliveEndpointCfg({ group: GROUP_02, name: "nginx (ping)", url: "http://my-smart-home" }),
+		createIsAliveEndpointCfg({ group: GROUP_02, name: "node-red (ping)", url: "http://my-smart-home:1880/ui" }),
+		createIsAliveEndpointCfg({ group: GROUP_02, name: "zigbee2mqtt (ping)", url: "http://my-smart-home:8099" }),
 
 		createEndpointCfgForHetznerStorageShare({
 			domain: `${runtimeEnv.domains[".de"].subDomains.nc.name}.${runtimeEnv.domains[".de"].name}`,
 			group: GROUP_03,
-			name: "nc.@.de",
+			name: "nc.@.de (DNS)",
 			storageShareID: runtimeEnv.domains[".de"].subDomains.nc.storageShareID,
 		}),
-
 		...createEndpointCfgListForProton({
 			domain: runtimeEnv.domains[".de"].name,
 			domainKey: runtimeEnv.domains[".de"].protonDomainKey,
 			group: GROUP_03,
-			name: "@.de",
+			name: "@.de (DNS)",
 		}),
 		...createEndpointCfgListForProton({
 			domain: `${runtimeEnv.domains[".de"].subDomains.nr.name}.${runtimeEnv.domains[".de"].name}`,
 			domainKey: runtimeEnv.domains[".de"].subDomains.nr.protonDomainKey,
 			group: GROUP_03,
-			name: "nr.@.de",
+			name: "nr.@.de (DNS)",
 		}),
-
 		...createEndpointCfgListForSimpleLogin({
 			domain: runtimeEnv.domains[".de"].name,
 			group: GROUP_03,
-			name: "ch.@.de",
+			name: "ch.@.de (DNS)",
 			subDomain: runtimeEnv.domains[".de"].subDomains.ch.name,
 		}),
 		...createEndpointCfgListForSimpleLogin({
 			domain: runtimeEnv.domains[".de"].name,
 			group: GROUP_03,
-			name: "sa.@.de",
+			name: "sa.@.de (DNS)",
 			subDomain: runtimeEnv.domains[".de"].subDomains.sa.name,
 		}),
+
 		...createEndpointCfgListForSimpleLogin({
 			domain: runtimeEnv.domains[".me"].name,
-			group: GROUP_03,
-			name: "@.me",
+			group: GROUP_04,
+			name: "@.me (DNS)",
 			subDomain: undefined,
 		}),
 		...createEndpointCfgListForSimpleLogin({
 			domain: runtimeEnv.domains[".me"].name,
-			group: GROUP_03,
-			name: "ch.@.me",
+			group: GROUP_04,
+			name: "ch.@.me (DNS)",
 			subDomain: runtimeEnv.domains[".me"].subDomains.ch.name,
 		}),
 		...createEndpointCfgListForSimpleLogin({
 			domain: runtimeEnv.domains[".me"].name,
-			group: GROUP_03,
-			name: "fa.@.me",
+			group: GROUP_04,
+			name: "fa.@.me (DNS)",
 			subDomain: runtimeEnv.domains[".me"].subDomains.fam.name,
 		}),
 		...createEndpointCfgListForSimpleLogin({
 			domain: runtimeEnv.domains[".me"].name,
-			group: GROUP_03,
-			name: "sa.@.me",
+			group: GROUP_04,
+			name: "sa.@.me (DNS)",
 			subDomain: runtimeEnv.domains[".me"].subDomains.sa.name,
 		}),
 	],
