49d2f1d8f504b2fa66a0e2dbf691c9ed2670e7ce
Christian Sporer <12594177+IanStorm@users.noreply.github.com>
2025-10-19 11:02:58 +0200

    docs: Add JSDoc based on ReadMe

--- src/gatus/alert.ts
+++ src/gatus/alert.ts
@@ -9,11 +9,44 @@ export type AlertConfigurationList = Array<AlertConfiguration>
  * 🔗 https://github.com/TwiN/gatus?tab=readme-ov-file#alerting
  */
 export interface AlertConfiguration {
+	/**
+	 * Description of the alert. Will be included in the alert sent.
+	 * @default ""
+	 */
 	description?: string
+
+	/**
+	 * Whether to enable the alert.
+	 * @default true
+	 */
 	enabled?: boolean
+
+	/**
+	 * Number of failures in a row needed before triggering the alert.
+	 * @default 3
+	 */
 	failureThreshold?: number
+
+	/**
+	 * Alerting provider configuration override for the given alert type
+	 * @default {}
+	 */
 	providerOverride?: unknown
+
+	/**
+	 * Whether to send a notification once a triggered alert is marked as resolved.
+	 * @default false
+	 */
 	sendOnResolved?: boolean
+
+	/**
+	 * Number of successes in a row before an ongoing incident is marked as resolved.
+	 * @default 2
+	 */
 	successThreshold?: number
+
+	/**
+	 * Type of alert.
+	 */
 	type: keyof AlertingConfiguration
 }
